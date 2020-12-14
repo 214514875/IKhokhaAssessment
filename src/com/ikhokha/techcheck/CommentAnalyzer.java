588bfb9ea5414032373287529be4604955b6ca95
Denver Pillay <denver2pillay2@gmail.com>
2020-12-14 21:53:11 +0200

    Added map counts instead of replacing, removed else statements

--- src/com/ikhokha/techcheck/CommentAnalyzer.java
+++ src/com/ikhokha/techcheck/CommentAnalyzer.java
@@ -9,37 +9,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommentAnalyzer {
-	
+
 	private File file;
-	
+
 	public CommentAnalyzer(File file) {
 		this.file = file;
 	}
-	
+
 	public Map<String, Integer> analyze() {
-		
+
 		Map<String, Integer> resultsMap = new HashMap<>();
-		
+
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			
+
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				
-				if (line.length() < 15) {
-					
-					incOccurrence(resultsMap, "SHORTER_THAN_15");
 
-				} else if (line.contains("Mover")) {
+				line = line.toLowerCase();
+
+				if (line.length() < 15) {
+
+					incOccurrence(resultsMap, "SHORTER_THAN_15");
+				}
+
+				if (line.contains("mover")) {
 
 					incOccurrence(resultsMap, "MOVER_MENTIONS");
-				
-				} else if (line.contains("Shaker")) {
+				}
+
+				if (line.contains("shaker")) {
 
 					incOccurrence(resultsMap, "SHAKER_MENTIONS");
-				
 				}
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found: " + file.getAbsolutePath());
 			e.printStackTrace();
@@ -47,18 +50,20 @@ public class CommentAnalyzer {
 			System.out.println("IO Error processing file: " + file.getAbsolutePath());
 			e.printStackTrace();
 		}
-		
+
 		return resultsMap;
-		
+
 	}
-	
+
 	/**
-	 * This method increments a counter by 1 for a match type on the countMap. Uninitialized keys will be set to 1
+	 * This method increments a counter by 1 for a match type on the countMap.
+	 * Uninitialized keys will be set to 1
+	 * 
 	 * @param countMap the map that keeps track of counts
-	 * @param key the key for the value to increment
+	 * @param key      the key for the value to increment
 	 */
 	private void incOccurrence(Map<String, Integer> countMap, String key) {
-		
+
 		countMap.putIfAbsent(key, 0);
 		countMap.put(key, countMap.get(key) + 1);
 	}
